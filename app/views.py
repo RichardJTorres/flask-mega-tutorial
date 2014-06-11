57c0f0d0d4af9475a4c74febe48cce87a108dff1
Rich Torres <rich@discovix.com>
2014-06-10 18:45:49 -0700

    adding some views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'a post'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'another post'
+        }
+    ]
+    return render_template("index.html", title='Home', user=user, posts=posts)
